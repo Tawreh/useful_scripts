310a08d1b0a9ee5221f117f715c08247ab34debf
Sophie Shanahan-Kluth <sophie@microserve.io>
2015-08-07 11:01:06 +0100

    Backup all databases in your mysql install.

---
+++ backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+clear
+
+# Database creds.
+user="root"
+pass="password"
+
+# Other options.
+backup_path="/home/sophie/Backups"
+date=$(date +"%Y-%m-%d")
+
+# Set default file permissions.
+umask 002
+
+# Get databases.
+databases=`mysql --user=${user} --password=${pass} -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+
+# Back up each one in turn.
+for db in $databases; do
+    if [[ "$db" != "information_schema" && "$db" != "performance_schema" && "$db" != "phpmyadmin" ]]; then
+        echo "Dumping database: $db"
+
+	# Dump the database.
+        mysqldump --opt --user=${user} --password=${pass} --databases ${db} > ${backup_path}/${date}-${db}.sql
+
+	# Show the user the result.
+	echo "Created backup: ${backup_path}/${date}-${db}.sql."
+    fi
+done
+
+# Remove database backups older than 7 days.
+find /home/sophie/Backups -mtime +7 -type f -delete
+echo "Deleted old databases."
